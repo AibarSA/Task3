4dba572aaecf651da09f7b2501aadadc0a1f02f0
Aibar-B <aibro_85@mail.ru>
2017-10-18 11:27:53 +0600

    add a sorting

--- src/main/java/HomeWork/Collection.java
+++ src/main/java/HomeWork/Collection.java
@@ -1,5 +1,6 @@
 package HomeWork;
 
+import java.util.Comparator;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -7,13 +8,20 @@ public class Collection {
 
     public static void main(String[] args) {
 
-        Set<String> strings = new TreeSet<String>();
+        Set<String> strings = new TreeSet<String>(new Comparator<String>() {
+            public int compare(String s1, String s2) {
+                return s1.compareTo(s2);
+            }
+        });
 
         for (int i = 0; i <args.length ; i++) {
             strings.add(args[i]);
         }
 
+
         System.out.println(strings);
 
     }
+
+
 }
